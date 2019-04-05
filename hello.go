4ff8fc8d4dae45b7dda250eb3fbdc2f6a2b2a9cc
dislersd <dislersd@gmail.com>
2019-04-05 13:32:22 -0700

    downloaded go with brew and created example folder in src

---
+++ hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// x := 10
+
+	// if x > 6 {
+	// 	fmt.Println("More than 6")
+	// } else if x < 6 {
+	// 	fmt.Println("Less than 6")
+	// } else {
+	// 	fmt.Println("Broken")
+	// }
+
+	a := []int{5, 4, 3, 2, 1}
+	a = append(a, 13)
+	fmt.Println(a)
+
+}
