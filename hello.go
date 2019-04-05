c0a29b58f22fb50bada1d40dccc630fda0a61aba
dislersd <dislersd@gmail.com>
2019-04-05 13:42:37 -0700

    added comments

--- hello.go
+++ hello.go
@@ -1,22 +1,26 @@
-package main
+package main // if you want to execute any go file must have package main
 
-import (
-	"fmt"
+import ( // import different packages here
+	"fmt" // i/o package
 )
 
-func main() {
-	// x := 10
+func main() { // func - go syntax - just like javascript - function name (params) {body}
 
-	// if x > 6 {
-	// 	fmt.Println("More than 6")
-	// } else if x < 6 {
-	// 	fmt.Println("Less than 6")
-	// } else {
-	// 	fmt.Println("Broken")
-	// }
+	var x int = 5 // declare types -> int, bool, string, etc..
+	fmt.Println(x)
 
-	a := []int{5, 4, 3, 2, 1}
+	y := 10 // go can infer types - this is shorthand for storing a variable
+
+	if y > 6 { // you can write if statement
+		fmt.Println("More than 6")
+	} else if y < 6 {
+		fmt.Println("Less than 6")
+	} else {
+		fmt.Println("Broken")
+	}
+
+	a := []int{5, 4, 3, 2, 1} // setting an array
 	a = append(a, 13)
-	fmt.Println(a)
+	fmt.Println(a) // => {5, 4, 3, 2, 1, 13}
 
 }
